c7fd2b49c81d15d15b4b9d97cdefb9a20388f367
MutasemShboul <62628198+Mutasemshboul@users.noreply.github.com>
2024-07-04 22:14:15 +0300

    Solve SQl Task

---
+++ TaskOne.sql
@@ -0,0 +1,50 @@
+create schema TaskOne
+
+CREATE TABLE TaskOne.Customers (
+    CustomerID INT IDENTITY(1,1) PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    PhoneNumber VARCHAR(20),
+    Address VARCHAR(255),
+    City VARCHAR(50),
+    State VARCHAR(50),
+    PostalCode VARCHAR(10),
+    Country VARCHAR(50),
+    CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE TaskOne.Orders (
+    OrderID INT IDENTITY(1,1) PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    ShippingAddress VARCHAR(255),
+    TotalAmount DECIMAL(10, 2),
+    Status VARCHAR(50),
+    FOREIGN KEY (CustomerID) REFERENCES TaskOne.Customers(CustomerID)
+);
+
+
+CREATE TABLE TaskOne.Products (
+    ProductID INT IDENTITY(1,1) PRIMARY KEY,
+    ProductName VARCHAR(100) NOT NULL,
+    Description TEXT,
+    Price DECIMAL(10, 2) NOT NULL,
+    StockQuantity INT,
+    CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE TaskOne.Employees (
+    EmployeeID INT IDENTITY(1,1) PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    PhoneNumber VARCHAR(20),
+    HireDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Position VARCHAR(50),
+    Salary DECIMAL(10, 2),
+    ManagerID INT,
+    FOREIGN KEY (ManagerID) REFERENCES TaskOne.Employees(EmployeeID)
+);
---
+++ TaskTwo.sql
@@ -0,0 +1,42 @@
+create schema TaskTwo;
+
+
+CREATE TABLE TaskTwo.Department (
+    departmentID INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(255) NOT NULL
+);
+
+CREATE TABLE TaskTwo.Users (
+    UsersID INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(255) NOT NULL,
+    birthdate DATE,
+    phone VARCHAR(15),
+    nationalIDs VARCHAR(20),
+    nationality NVARCHAR(255),
+    marital_statuses NVARCHAR(255),
+    personal_photo VARBINARY(MAX), 
+    entry_date DATE,
+    password VARBINARY(256), 
+    department_ID INT,
+    FOREIGN KEY (department_ID) REFERENCES TaskTwo.Department(departmentID)
+);
+
+CREATE TABLE TaskTwo.Task (
+    TaskID INT PRIMARY KEY IDENTITY(1,1),
+    UsersID INT,
+    title NVARCHAR(255) NOT NULL,
+    start_date DATE,
+    due_date DATE,
+    task_description NVARCHAR(1000),
+    level_of_importance INT,
+    task_status NVARCHAR(100), 
+    FOREIGN KEY (UsersID) REFERENCES TaskTwo.Users(UsersID)
+);
+
+CREATE TABLE TaskTwo.Feedback (
+    FeedbackID INT PRIMARY KEY IDENTITY(1,1), 
+    Name NVARCHAR(255),
+    Email NVARCHAR(255),
+    Subject NVARCHAR(255),
+    Message NVARCHAR(1000)
+);
